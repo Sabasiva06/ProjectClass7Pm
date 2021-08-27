3c01f36013c6e78faf342a6e240305bcd9d976f6
Sarath13-dev <ramsssjrs@gmail.com>
2021-08-27 19:21:42 +0530

    master

---
+++ src/org/login/Employee.java
@@ -0,0 +1,27 @@
+package org.login;
+
+//Class
+public class Employee {
+
+	public static void main(String[] args) {
+		String s = "welcome java in greens technology";
+
+		String[] split = s.split(" ");
+		String output = "";
+		for (int i = 0; i < split.length; i++) {
+			if (i % 2 == 0) {
+				for (int j = 0; j < split.length; j++) {
+					String string = split[j];
+					output = output + string;
+				}
+			} else {
+
+				output = output + split[i];
+
+			}
+			output = output + " ";
+		}
+		System.out.println(output);
+	}
+
+}
